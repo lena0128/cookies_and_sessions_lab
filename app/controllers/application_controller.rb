549283cabed5c726bc7d8d3b95a6e659bc0ae6cf
Lena Hou <lenahou2013@gmail.com>
2021-08-07 13:57:14 -0400

    commit tree a09c413ba5bb31a4c98695cde030af5c6b63d80e

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,12 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  # helper_method helps us to access to a controller method from within a view
+  helper_method :cart
+
+# the cart method should utilize the Rails session method and return an array of the items stored in the cart   
+  def cart
+    session[:cart] ||= [ ]
+  end
 end
